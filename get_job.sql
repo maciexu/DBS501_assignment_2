a0ecf214ff4425cb6cabe52b628747771c2b11c2
maciexu <43970987+maciexu@users.noreply.github.com>
2020-12-08 02:15:00 -0500

    Update get_job.sql

--- get_job.sql
+++ get_job.sql
@@ -9,6 +9,13 @@ BEGIN
   where job_id = j_id;
   RETURN j_title;
 END GET_JOB;
+/
 
+variable j_id VARCHAR2(10)
+variable j_title VARCHAR2(35)
+execute :j_id :='SA_REP'
+execute get_job(:j_id)
+print j_id
+print j_title
 
 
